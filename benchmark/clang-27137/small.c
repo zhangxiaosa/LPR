663b44d0efa4d50045651b71507ace7db27d6b0a
zhangxiaosa <hustzmx@gmail.com>
2023-06-12 21:01:37 -0400

    27137

--- benchmark/clang-27137/small.c
+++ benchmark/clang-27137/small.c
@@ -3,16 +3,16 @@ typedef short int16_t;
 typedef int int32_t;
 typedef unsigned uint32_t;
 typedef unsigned uint64_t;
-void fn1(uint32_t crc, int flag) {
+void platform_main_end(uint32_t crc, int flag) {
   printf("checksum = %X\n", crc);
 }
 uint32_t crc32_context;
-void fn2(b) { crc32_context = b; }
-void fn3(val) { fn2(val); }
-void fn4(uint64_t val, char *vname, int flag) { fn3(val); }
+void crc32_byte(b) { crc32_context = b; }
+void crc32_8bytes(val) { crc32_byte(val); }
+void transparent_crc(uint64_t val, char *vname, int flag) { crc32_8bytes(val); }
 int32_t g_573;
 int8_t g_4689;
-int8_t fn5() {
+int8_t func_1() {
   int16_t l_5913;
   int i;
   int32_t BGCGCl_6280 = g_573;
@@ -26,9 +26,8 @@ int8_t fn5() {
 }
 int main() {
   int print_hash_value = 0;
-  fn5();
-  fn4(g_4689, "g_4689", print_hash_value);
-  fn1(crc32_context, print_hash_value);
+  func_1();
+  transparent_crc(g_4689, "g_4689", print_hash_value);
+  platform_main_end(crc32_context, print_hash_value);
   return 0;
 }
-
