760ad88f42fce26f32067d57bd8201213602c6b6
zhangxiaosa <hustzmx@gmail.com>
2023-06-20 20:59:37 -0400

    26350 rm

--- batch.sh
+++ batch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cases=(clang-22382 clang-22704 clang-23309 clang-23353 clang-25900 clang-26350 clang-26760 clang-27137 clang-27747 clang-31259 gcc-59903 gcc-60116 gcc-61383 gcc-61917 gcc-64990 gcc-65383 gcc-66186 gcc-66375 gcc-70127 gcc-70586 gcc-71626)
+cases=(clang-22382 clang-22704 clang-23309 clang-23353 clang-25900 clang-26760 clang-27137 clang-27747 clang-31259 gcc-59903 gcc-60116 gcc-61383 gcc-61917 gcc-64990 gcc-65383 gcc-66186 gcc-66375 gcc-70127 gcc-70586 gcc-71626)
 
 for case in "${cases[@]}"
 do
