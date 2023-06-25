11d3378325461343a8688a5643b225eeb864a466
zhangxiaosa <hustzmx@gmail.com>
2023-06-25 15:31:15 -0400

    update

--- batch_rust.sh
+++ batch_rust.sh
@@ -5,6 +5,6 @@ cases=(rust-44800 rust-69039 rust-77002 rust-77320 rust-77323 rust-77910 rust-77
 for case in "${cases[@]}"
 do
     echo "start $case"
-    python3 perses_gpt.py --benchmark_suite ./benchmark_suites/rust/perses_result --case $case $1
+    python3 perses_gpt.py --prompts configuration_rust.json --benchmark_suite ./benchmark_suites/rust/perses_result --case $case $1
     echo -e "\n\n\n"
 done
