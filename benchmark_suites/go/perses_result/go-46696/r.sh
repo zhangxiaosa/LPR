1dce5684228c20f3bc421c811cdb2950dde91f5c
zhangxiaosa <hustzmx@gmail.com>
2023-06-29 23:31:51 -0400

    update go-46696

--- benchmark_suites/go/perses_result/go-46696/r.sh
+++ benchmark_suites/go/perses_result/go-46696/r.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-gvm use go1.17beta1
-
+# good version
+gvm use go1.16
 go version > version.log
+if !(grep -q "go1.16" version.log); then
+  exit 1
+fi
+(go run small.go || true) > run.log 2>&1
 
-if !(grep -q "go1.17beta1" version.log); then
+if (grep -q "panic" run.log); then
   exit 1
 fi
 
+# buggy version
+gvm use go1.17beta1
+go version > version.log
+if !(grep -q "go1.17beta1" version.log); then
+  exit 1
+fi
 (go run small.go || true) > run.log 2>&1
 
 if !(grep -q "panic" run.log); then
