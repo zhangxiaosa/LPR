11b2663c130a0da047f31c7f3f4c8248da9c7073
zhangxiaosa <hustzmx@gmail.com>
2023-11-07 17:36:15 +0000

    add js-4

---
+++ benchmark_suites/javascript/js-4/small.js
@@ -0,0 +1,26 @@
+function opt(opt_param){
+const v2 = Int16Array.from(RangeError);
+const v4 = [];
+const v6 = v4.__proto__;
+const v7 = v6.unshift(4294967297);
+v6.length = 256;
+const v8 = [];
+const v9 = v8.__proto__;
+const v10 = v9.pop();
+const v11 = [];
+const v12 = v11.splice();
+try {
+    const v13 = -541246833n;
+    const v14 = v2.lastIndexOf(RangeError,v2);
+} catch(v15) {
+}
+const v16 = v8[RangeError];
+const v17 = [256,256,256,256,256];
+const v18 = v4.length;
+const v20 = Infinity.toLocaleString();
+const v22 = Infinity + v20;
+const v24 = JSON.stringify(v22);
+const v25 = eval(v24);
+const v26 = eval(v10);
+return v26;
+}
