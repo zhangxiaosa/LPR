666858acad538d808da5e98fb19210e391191bc5
zhangxiaosa <hustzmx@gmail.com>
2023-11-07 17:31:36 +0000

    add js1

---
+++ benchmark_suites/javascript/js-1/small.js
@@ -0,0 +1,38 @@
+function opt(opt_param){
+const v1 = class V1 extends Uint8ClampedArray {
+    constructor(v3,v4,v5,v6) {
+        super();
+    }
+    pow(v8,v9) {
+    }
+    isView(v11,v12,v13) {
+        const v14 = [];
+        const v16 = v14.unshift(-3848398693);
+        const v18 = v14.unshift(-1416346882);
+        const v19 = v14.unshift(v16);
+        const v21 = new Float64Array(v14);
+        const v22 = v21.sort();
+    }
+    join(v24,v25,v26,v27) {
+    }
+};
+try {
+    const v28 = new v1(Uint8ClampedArray);
+    const v30 = v28.min(-878583891n);
+} catch(v31) {
+}
+const v32 = [Uint8ClampedArray];
+const v33 = v32.__proto__;
+const v34 = v33.unshift(v32);
+const v35 = v32.toLocaleString();
+const v36 = v35.padStart(v34);
+const v37 = /p*/mygi;
+try {
+    const v40 = [-3028485467,v36,-3028485467];
+    const v41 = v40.join(v37);
+    const v42 = Function(v41,Function);
+} catch(v43) {
+} finally {
+}
+return v36;
+}
