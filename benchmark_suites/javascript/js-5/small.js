3927266ebc99c46f8309d889e50730d24efff662
zhangxiaosa <hustzmx@gmail.com>
2023-11-07 17:37:46 +0000

    add js-5

---
+++ benchmark_suites/javascript/js-5/small.js
@@ -0,0 +1,28 @@
+function opt(opt_param){
+function v0(v1,v2,v3) {
+}
+const v5 = eval(v0);
+let v6 = {};
+let v7 = Proxy;
+let v8 = new v7(v5,v6);
+let v9 = "";
+v8.prototype = v9;
+v7 = eval;
+v9 /= v0;
+({"arguments":v6,"byteOffset":v8,"prototype":v0,"source":v7,"toString":v8,} = v5);
+const v11 = class V11 extends Map {
+    constructor(v13,v14,v15,v16) {
+        super();
+    }
+    pow(v18,v19) {
+    }
+    isView(v21,v22,v23) {
+    }
+    join(v25,v26,v27,v28) {
+    }
+};
+const v29 = new v11();
+const v30 = new Map(v29);
+const v31 = eval(v0);
+return v31;
+}
