084f014a4e76f0d476033b654ba240792d6d2517
zhangxiaosa <hustzmx@gmail.com>
2023-11-07 17:33:14 +0000

    add js2

---
+++ benchmark_suites/javascript/js-2/small.js
@@ -0,0 +1,38 @@
+function opt(opt_param){
+const v0 = [];
+const v2 = v0.__proto__;
+const v3 = v2.unshift(4294967297,v2);
+const v4 = -693667.7563190323;
+const v5 = v3.toLocaleString();
+const v6 = v5.replace(v3);
+try {
+    const v7 = 54388.265657296404;
+    const v8 = v6.toLocaleString();
+    const v9 = `
+        const v10 = /P/gis;
+        const v12 = class V12 {
+            constructor(v14,...v15) {
+            }
+            toLocaleString(v17,v18,v19) {
+            }
+            setPrototypeOf(v21,v22,v23,v24) {
+            }
+            trunc(v26,v27,v28,v29) {
+            }
+        };
+        const v30 = parseFloat(v5);
+        const v31 = v9.replace(v10,v8);
+        try {
+            const v33 = eval(v31);
+        } catch(v34) {
+        } finally {
+        }
+    `;
+    const v36 = eval(v9);
+    const v37 = eval;
+    const v38 = eval(v6);
+} catch(v39) {
+} finally {
+}
+return v6;
+}
