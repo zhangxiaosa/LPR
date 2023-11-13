34472e78f45b7fb522a8d5ed5a1dd173b435d169
zhangxiaosa <hustzmx@gmail.com>
2023-11-13 16:35:21 +0000

    rm old perses results of c

--- benchmark_suites/c/creduce_results/clang-26760/small.c
+++
@@ -1,9 +0,0 @@
-char a;
-void b() {
-  unsigned c = 61;
-  int d = 0 >> a && c;
-e:
-  c = c >> 1 << ~d;
-  goto e;
-}
-int main() {}
--- benchmark_suites/c/creduce_results/gcc-71626/small.c
+++
@@ -1,8 +0,0 @@
-typedef long a;
-typedef a c __attribute__((__vector_size__(sizeof(a))));
-b() {}
-c d() {
-  c e = {b};
-  return e;
-}
-main() {}
