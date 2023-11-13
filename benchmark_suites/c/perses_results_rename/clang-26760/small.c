4715d8ffb251798b4bb46f810e0f64f781d08ed5
zhangxiaosa <hustzmx@gmail.com>
2023-11-13 02:43:43 +0000

    add folder perses_results_rename

---
+++ benchmark_suites/c/perses_results_rename/clang-26760/small.c
@@ -0,0 +1,29 @@
+typedef signed a;
+typedef short b;
+typedef int c;
+typedef unsigned d;
+a e;
+b fn1() {
+  a g;
+  c h = 0x24F96B7BL;
+  d i;
+  if (g)
+    for (;;)
+      break;
+  else
+    for (; h; --h)
+      ;
+  c j = 1L;
+  c k = h;
+  a l = e;
+  c m = l;
+  d n = m;
+o:;
+  i = i >> j;
+  g = n;
+  if (g) {
+    i = i << k;
+    goto o;
+  }
+}
+int main() {}
