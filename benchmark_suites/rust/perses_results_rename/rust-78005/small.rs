00200e0474b6b872312d8e2d9bc3581f094408b0
zhangxiaosa <hustzmx@gmail.com>
2023-11-13 02:53:26 +0000

    add perses_results_rename for rust

---
+++ benchmark_suites/rust/perses_results_rename/rust-78005/small.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let a: b<b>;
+    let a: c<b>;
+    let a: d<b>;
+    let a: e<b>;
+    let a: f<b>;
+    let a: usize<main>;
+    let a;
+    let a;
+    let a;
+    let a;
+    let a;
+    let a;
+    let a;
+    let a;
+    let a;
+    let a;
+    let a;
+    let a;
+    let a;
+    let a;
+    let a;
+    let a;
+}
