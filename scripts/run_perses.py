661e0877af78695f799ca7c4a43a7c94f17325c9
zhangxiaosa <hustzmx@gmail.com>
2023-11-13 15:24:30 -0500

    add error handling

--- scripts/run_perses.py
+++ scripts/run_perses.py
@@ -4,41 +4,41 @@ import shutil
 from concurrent.futures import ProcessPoolExecutor
 
 def process_case(case, benchmark_suite_folder, code_version, args_string):
-    main_folder = os.path.normpath(
-        os.path.join("/tmp/gpt_reduction/results/", code_version, args_string, case)
-    )
-    os.makedirs(main_folder, exist_ok=True)
+    try:
+        main_folder = os.path.normpath(
+            os.path.join("/tmp/gpt_reduction/results/", code_version, args_string, case)
+        )
+        os.makedirs(main_folder, exist_ok=True)
 
-    original_program_path = os.path.join(benchmark_suite_folder, case, utils.PROGRAM_NAME)
-    original_script_path = os.path.join(benchmark_suite_folder, case, utils.SCRIPT_NAME)
-    
-    main_program_path = os.path.join(main_folder, utils.PROGRAM_NAME)
-    main_script_path = os.path.join(main_folder, utils.SCRIPT_NAME)
+        original_program_path = os.path.join(benchmark_suite_folder, case, utils.PROGRAM_NAME)
+        original_script_path = os.path.join(benchmark_suite_folder, case, utils.SCRIPT_NAME)
+        
+        main_program_path = os.path.join(main_folder, utils.PROGRAM_NAME)
+        main_script_path = os.path.join(main_folder, utils.SCRIPT_NAME)
 
-    shutil.copy(original_program_path, main_program_path)
-    shutil.copy(original_script_path, main_script_path)
-    print(main_folder)
-    
-    utils.print_and_log(f"Start reduction on benchmark {case}", level=0)
-    utils.call_perses(main_folder, level=0)
-    utils.print_and_log(f"Finished reduction on benchmark {case}", level=0)
+        shutil.copy(original_program_path, main_program_path)
+        shutil.copy(original_script_path, main_script_path)
+        print(f"Processing in folder: {main_folder}")
+        
+        utils.print_and_log(f"Start reduction on benchmark {case}", level=0)
+        utils.call_perses(main_folder, level=0)
+        utils.print_and_log(f"Finished reduction on benchmark {case}", level=0)
+    except Exception as e:
+        print(f"Error processing case {case}: {e}")
 
 def main():
     args = utils.initialize_args()
-    print(args)
     args_string = utils.get_args_string(args)
 
     case = args.case
     benchmark_suite_folder = args.benchmark_suite
-    print(benchmark_suite_folder)
 
     code_version = utils.get_current_version()
     case_list = utils.get_benchmarks(benchmark_suite_folder, case)
 
     max_jobs = args.max_jobs
-    print(max_jobs)
     with ProcessPoolExecutor(max_workers=max_jobs) as executor:
-        futures = [executor.submit(process_case, case, benchmark_suite_folder, code_version, args_string) for case in case_list]
+        futures = [executor.submit(process_case, c, benchmark_suite_folder, code_version, args_string) for c in case_list]
         for future in futures:
             future.result()
 
