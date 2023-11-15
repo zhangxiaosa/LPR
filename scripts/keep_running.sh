960ef73ff82da0889bafe42ff34ccf722a6082f9
zhangxiaosa <hustzmx@gmail.com>
2023-11-14 20:04:26 -0500

    add keep_running.sh

---
+++ scripts/keep_running.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Replace the following with the command you want to run in a loop
+COMMAND="python scripts/run_gpt_reduction.py --benchmark-suite /tmp/gpt_reduction/benchmark_suites/c/perses_results_rename --id 0"
+
+# Loop runs the command until it returns 0
+while ! $COMMAND; do
+    echo "Command failed, retrying..."
+    # Optional: Add a sleep command to pause for a while, like: sleep 1
+done
+
+echo "Command executed successfully with exit status 0."
+
