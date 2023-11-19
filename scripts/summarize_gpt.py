3b72b5ec947b93e9cad968c08f937aabadc5290f
zhangxiaosa <hustzmx@gmail.com>
2023-11-18 23:25:43 -0500

    finish summarize_gpt.py

--- scripts/summarize_gpt.py
+++ scripts/summarize_gpt.py
@@ -28,12 +28,38 @@ def get_token_num_from_log(file):
     except FileNotFoundError:
         return None
 
+# Function to count the number of 'trial' folders and sum the execution counts from perses_log.txt
+def get_query_number(case_path):
+    trial_count = 0
+    perses_execution_count_sum = 0
+
+    for root, dirs, files in os.walk(case_path):
+        # Count trial folders
+        trial_count += len([d for d in dirs if 'trial' in d])
+        # Sum execution counts from perses_log.txt
+        for file in files:
+            if file == 'perses_log.txt':
+                with open(os.path.join(root, file), 'r') as f:
+                    last_line = f.readlines()[-1]
+                    match = re.search(r'Test script execution count: (\d+)', last_line)
+                    if match:
+                        perses_execution_count_sum += int(match.group(1))
+
+    return trial_count + perses_execution_count_sum
+
+# Function to count the number of folders containing 'iteration'
+def get_iteration_count(case_path):
+    iteration_count = 0
+    for root, dirs, files in os.walk(case_path):
+        iteration_count += len([d for d in dirs if 'iteration' in d])
+    return iteration_count
+
 RESULT_PATH = sys.argv[1]
 benchmark_suite = utils.determine_benchmark_suite(RESULT_PATH)
 
 with open(os.path.join(RESULT_PATH, 'summary.csv'), 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(["target", "time", "token num"])
+    csv_writer.writerow(["target", "time", "token num", "query number", "iteration"])
 
     for target in benchmark_suite:
         row = [target]  # Initialize row with target as the first column
@@ -41,7 +67,7 @@ with open(os.path.join(RESULT_PATH, 'summary.csv'), 'w', newline='') as csvfile:
 
         if not os.path.exists(case_path):
             print(f"{target} is not available")
-            csv_writer.writerow([target, None, None])  # Write only target if not available
+            csv_writer.writerow([target, None, None, None, None])  # Write only target if not available
             continue
 
         # Calculate time
@@ -51,11 +77,16 @@ with open(os.path.join(RESULT_PATH, 'summary.csv'), 'w', newline='') as csvfile:
         log_file = os.path.join(case_path, "log.txt")
         token_num = get_token_num_from_log(log_file)
 
+        # Calculate query number
+        query_number = get_query_number(case_path)
+
+        # Calculate iteration count
+        iteration_count = get_iteration_count(case_path)
+
         if token_num is None:
             print(f"{target}: token number not available")
-            csv_writer.writerow([target, None, None])
+            csv_writer.writerow([target, None, None, None, None])
         else:
-            print(f"target: {target}: time: {time}, token num: {token_num}")
-            row.extend([time, token_num])
+            print(f"target: {target}: time: {time}, token num: {token_num}, query number: {query_number}, iteration: {iteration_count}")
+            row.extend([time, token_num, query_number, iteration_count])
             csv_writer.writerow(row)
-
