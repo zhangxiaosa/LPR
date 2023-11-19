0a7bbab36d9f34219b5a54f7635e6c608d14a08b
zhangxiaosa <hustzmx@gmail.com>
2023-11-19 00:01:55 -0500

    change target list strategy

--- scripts/run_gpt_reduction.py
+++ scripts/run_gpt_reduction.py
@@ -44,26 +44,21 @@ def call_gpt_with_multi_level_prompt(prompts, operation, output_folder, llm_vers
         # save response time
         utils.save_file(operation_folder, "primary_question_response_time.txt", f"{end_time-start_time:.2f}")
 
-        # try multiple times to ensure the quality of target_list
-        longest_current_list = []
-
+        # Iterating through the trials
         for trial in range(trial_number):
             response_text = completion.choices[trial].message.content
             response_json = utils.extract_json(response_text)
-            
+
             if "target_list" in response_json:
                 current_list = response_json["target_list"]
-                
-                # Deduplicate and filter out non-string elements
+
+                # Check if current_list is a list, deduplicate and filter out non-string elements
                 if isinstance(current_list, list):
                     deduplicated_and_filtered_list = [item for item in set(current_list) if isinstance(item, str)]
-                    
-                    # Update the longest list if current one is longer
-                    if len(deduplicated_and_filtered_list) > len(longest_current_list):
-                        longest_current_list = deduplicated_and_filtered_list
 
-        # Sort the longest deduplicated and filtered list
-        target_list = sorted(longest_current_list)
+                    # Use the deduplicated and filtered list from the current trial
+                    target_list = sorted(deduplicated_and_filtered_list)
+                    break  # Break the loop as we have found our list
 
         print_and_log(f"Primary question finished in {end_time-start_time:.2f} seconds", level=level)
         utils.save_file(operation_folder, "finish", f"{end_time-start_time:.2f}")
