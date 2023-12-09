0fe575800a3bb1a48c9a39589e8baf9f17990845
zhangxiaosa <hustzmx@gmail.com>
2023-12-09 15:56:15 -0800

    gcc-66186: grep segmentation fault

--- benchmark_suites/c/original/gcc-66186/r.sh
+++ benchmark_suites/c/original/gcc-66186/r.sh
@@ -164,12 +164,6 @@ for cc in "${BADCC2[@]}" ; do
         if [ "$ret" -ne 137 ] ; then
             exit 1
         fi
-
-        # grep error message
-        (sh -c ./t) > out2.txt 2>&1
-        if ! grep -e "Segmentation fault" out2.txt; then
-          exit 1
-        fi
     done
 done
 
@@ -188,10 +182,14 @@ for cc in "${BADCC3[@]}" ; do
         # the possibility of producing uncertain result
         segfault_happens=false
         for _ in {1..6} ; do
-            (timeout -s 9 $TIMEOUTEXE ./t >out2.txt 2>&1) >&/dev/null
+            (timeout -s 9 $TIMEOUTEXE ./t) >&/dev/null
             ret=$?
             if [ "$ret" == 139 ] ; then
-                segfault_happens=true
+                # grep error message
+                (sh -c ./t) > out2.txt 2>&1
+                if grep -e "Segmentation fault" out2.txt; then
+                  segfault_happens=true
+                fi
                 # "break" is not used here for stable execution time
             fi
         done
